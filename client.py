a40531f599dd268e5d8b0db0933b5b95cfc3829c
Rubenoo <ruben.oosterink@gmail.com>
2018-10-25 13:07:05 +0200

    code cleanup

--- client.py
+++ client.py
@@ -2,33 +2,24 @@ from urllib.request import urlopen
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+hostname='192.168.42.5:5000'
 
-# constanten: hostname van de server en definitie van de knop
-hostname='192.168.42.5:5000' # vul hier de juiste hostname en poort in
-
-# hulpfunctie voor sturen van een bericht naar de server
-#    het antwoord wordt simpelweg geprint
 def httpconnect(action):
-    url='http://{}/{}'.format(hostname,action)
+    url='http://{}/{}'.format(hostname, action)
     try:
         print(urlopen(url).read().decode())
     except:
         print("Verbinding naar {} kon niet gemaakt worden".format(url))
         exit()
 
-# actie voor ingedrukte knop (voor gebruik met gpiozero geen argument)
-def knopin():
-    httpconnect('knopin')
+def alarmAan():
+    httpconnect('alarmAan')
 
-# klaar voor gebruik, open de basis URL
 httpconnect('')
 
-# blijf oneindig deze lus maken, waarbij input gelezen wordt
-#     en de functie knopin() aangeroepen bij invoer van '+' (gevolgd door enter)
-#     en de functie knopuit() bij invoer van '-' (gevolgd door enter)
 while True:
-    invoer=input()
-    if invoer == '+' or GPIO.input(24) == 0:
-        knopin()
+    print(GPIO.input(21))
+    if GPIO.input(21) == 1:
+        alarmAan()
 GPIO.cleanup()
--- server.py
+++ server.py
@@ -2,23 +2,14 @@ from flask import Flask
 import RPi.GPIO as GPIO
 import time
 
-
-# constanten: de definitie van de Flask applicatie en de definitie van de LED
 app = Flask(__name__)   # standaard gebruik van Flask
-# de route bepaalt bij welk pad de onderstaande callback hoort
 @app.route("/")
-# functie die wanneer de knop is ingedrukt de LED aanzet
-#     en een bevestiging geeft naar de client
 def ready():
-    # voer hier programmacode toe die van belang is
-    #     wanneer de client zich voor het eerst meldt
     return "Server beschikbaar\n"
 
-# de route bepaalt bij welk pad de onderstaande callback hoort
-@app.route("/knopin")
-# functie die wanneer de knop is ingedrukt de LED aanzet
-#     en een bevestiging geeft naar de client
-def knopin():
+@app.route("/alarmAaan")
+
+def alarmAan():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(18, GPIO.OUT)
     GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
@@ -35,10 +26,6 @@ def knopin():
             GPIO.cleanup()
             break
     return "het alarm is uitgeschakeld\n"
-    # voer hier programmacode toe die van belang is
-    #     wanneer de client een knopdruk aangeeft
 
-# manier om Flask vanuit Python te laten starten
-#    host='0.0.0.0' zorgt ervoor dat op alle NICs geluisterd wordt
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
