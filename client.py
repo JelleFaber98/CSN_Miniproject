7972c184367e04619e871bdc2b2ef4952ec99c42
Rubenoo <ruben.oosterink@gmail.com>
2018-10-25 14:56:05 +0200

    Comments toegevoegd

--- client.py
+++ client.py
@@ -2,23 +2,23 @@ from urllib.request import urlopen
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-hostname='192.168.42.5:5000'
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Hier word de knop geconfigureerd
+hostname='192.168.42.5:5000'  # Dit is het IP adres van de server
 
 def httpconnect(action):
     url='http://{}/{}'.format(hostname, action)
     try:
         print(urlopen(url).read().decode())
     except:
-        print("Verbinding naar {} kon niet gemaakt worden".format(url))
+        print("Verbinding naar {} kon niet gemaakt worden".format(url))  # Als je niet kan verbinden met de server word deze melding gegeven
         exit()
 
 def alarmAan():
-    httpconnect('alarmAan')
+    httpconnect('alarmAan')  # Hiermee word er verbinding gemaakt met de server en word de alarmAan functie aangeroepen op de server
 
 httpconnect('')
 
 while True:
-    if GPIO.input(21) == 1:
+    if GPIO.input(21) == 1:  # Hier word gekeken of de knop is ingedrukt
         alarmAan()
 GPIO.cleanup()
--- server.py
+++ server.py
@@ -2,28 +2,32 @@ from flask import Flask
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)  # Hier word het lampje geconfigureerd
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)   # Hier word de knop mee geconfigureerd
+GPIO.setup(17, GPIO.OUT)  # Hier word de beeper geconfigureerd
+
 app = Flask(__name__)
+
 @app.route("/")
+
 def ready():
     return "Server beschikbaar\n"
 
 @app.route("/alarmAan")
 
 def alarmAan():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(18, GPIO.OUT)
-    GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(17, GPIO.OUT)
+
     while True:
-        if GPIO.input(24) == 0:
-            GPIO.output(18, GPIO.HIGH)
-            GPIO.output(17, GPIO.HIGH)
-            time.sleep(1)
-            GPIO.output(18, GPIO.LOW)
-            GPIO.output(17, GPIO.LOW)
-            time.sleep(1)
+        if GPIO.input(24) == 0: # Hier word gekeken of de knop niet is ingedrukt
+            GPIO.output(18, GPIO.HIGH)  # Hier gaat het lampje branden
+            GPIO.output(17, GPIO.HIGH)  # Hier gaat de beeper aan
+            time.sleep(1)  # Hier komt een pauze van 1 seconden
+            GPIO.output(18, GPIO.LOW)  # Hier gaat het lampje uit
+            GPIO.output(17, GPIO.LOW)  # Hier gaat de beeper uit
+            time.sleep(1)  # Hier komt een pauze van 1 seconden
         else:
-            GPIO.cleanup()
+            GPIO.cleanup()  # Hier word de GPIO opgeruimd
             break
     return "het alarm is uitgeschakeld\n"
 
